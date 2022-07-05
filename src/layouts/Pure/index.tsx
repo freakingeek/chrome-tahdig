929f5ef4bb26bb5d0ae38552034452ba4fe0f218
Yasin Silavi <ys.silavi@gmail.com>
2022-07-05 11:09:32 +0430

    feat: add custom class to pure layout

--- src/layouts/Pure/index.tsx
+++ src/layouts/Pure/index.tsx
@@ -1,7 +1,12 @@
 import styles from './Pure.module.scss';
 
-function Pure({ children }: React.PropsWithChildren) {
-  return <div className={styles.Pure}>{children}</div>;
+interface PurePropTypes {
+  className: string;
+  children: React.ReactNode;
+}
+
+function Pure({ className, children }: PurePropTypes) {
+  return <div className={`${styles.Pure} ${className}`}>{children}</div>;
 }
 
 export default Pure;
